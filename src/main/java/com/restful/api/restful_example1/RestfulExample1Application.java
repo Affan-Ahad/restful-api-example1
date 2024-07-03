4f3e93279ebfd479fd58a37e8aea85f68a21c13e
Affan-Ahad <affanahad870@gmail.com>
2024-07-02 20:59:22 -0700

    Added initial code

---
+++ src/main/java/com/restful/api/restful_example1/RestfulExample1Application.java
@@ -0,0 +1,13 @@
+package com.restful.api.restful_example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulExample1Application.class, args);
+	}
+
+}
